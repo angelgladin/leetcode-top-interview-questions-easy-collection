003b8e6942fd067458c60f95c71faf22e91d5ae9
Angel Gladin <xyz@angelgladin.xyz>
2023-01-02 14:05:45 -0600

    Remove Duplicates from Sorted Array :heavy_check_mark:

--- array/remove-duplicates-from-sorted-array.ts
+++ array/remove-duplicates-from-sorted-array.ts
@@ -1,1 +1,12 @@
 // Remove Duplicates from Sorted Array
+
+function removeDuplicates(nums: number[]): number {
+    let insertingIndex = 1;
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[insertingIndex - 1] != nums[i]) {
+            nums[insertingIndex] = nums[i];
+            insertingIndex++;
+        }
+    }
+    return insertingIndex;
+};
